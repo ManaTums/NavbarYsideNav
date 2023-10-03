433249db5b4eb56c7066e42d802db1e6a4080623
ManaTums <snchez0407@gmail.com>
2023-10-03 09:36:36 -0700

    se corrigio header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './Componentes/header/header.component';
 import { SideNavComponent } from './Componentes/side-nav/side-nav.component';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatListModule} from '@angular/material/list';
-import {MatIconModule} from '@angular/material/icon';
-import { MatTabsModule } from '@angular/material/tabs';
+//ANGULAR MATERIAL
+import { AngularMaterialModule } from './angular-material.module';
+
 import { HomeComponent } from './Web/home/home.component';
 import { NosotrosComponent } from './Web/nosotros/nosotros.component';
 import { ContactoComponent } from './Web/contacto/contacto.component';
-
-
 
 import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
 import { ModalComponent } from './Componentes/modal/modal.component';
@@ -39,14 +34,9 @@ import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-MatSidenavModule,
-MatMenuModule,
-MatListModule,
-MatIconModule,
-MatTabsModule,
-MdbModalModule,
-MdbCarouselModule
+    AngularMaterialModule,
+    MdbModalModule,
+    MdbCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
